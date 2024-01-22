825ac5f71c95f3820cee71453073a08b8e4b6ae2
TATO <tatososelia93@gmail.com>
2024-01-22 18:47:48 +0400

    feat: add test for recipe detail

--- src/apps/recipe/tests/test_recipe_api.py
+++ src/apps/recipe/tests/test_recipe_api.py
@@ -7,12 +7,17 @@ from django.urls import reverse
 from apps.recipe.models import Recipe
 from apps.user.factories import UserFactory
 from apps.recipe.factories import RecipeFactory
-from apps.recipe.serializers import RecipeSerializer
+from apps.recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 
 from rest_framework import status
 from rest_framework.test import APIClient
 
 RECIPE_URL = reverse('recipe:recipe-list')
+
+
+def detail_url(recipe_id):
+    """Create and return a recipe detail URL."""
+    return reverse('recipe:recipe-detail', args=[recipe_id])
 
 
 class PublicRecipeApiTests(TestCase):
@@ -57,4 +62,14 @@ class PrivateRecipeApiTest(TestCase):
         recipes = Recipe.objects.filter(user=self.user).order_by('-id')
         serializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_get_recipe_detail(self):
+        """Test get recipe detail."""
+        recipe = RecipeFactory.create_batch(1, user=self.user)
+
+        url = detail_url(recipe[0].id)
+        res = self.client.get(url)
+
+        serializer = RecipeDetailSerializer(recipe)
         self.assertEqual(res.data, serializer.data)
