ecf7d95a0e62c5ca82c3d4cbae65fb38d79ba238
TATO <tatososelia93@gmail.com>
2024-01-24 19:14:50 +0400

    feat: add TagFactory

--- src/apps/recipe/factories.py
+++ src/apps/recipe/factories.py
@@ -14,3 +14,12 @@ class RecipeFactory(DjangoModelFactory):
     time_minutes = factory.Faker('random_int', min=5, max=120)
     price = factory.Faker('pydecimal', left_digits=2, right_digits=2)
     link = factory.Faker('url')
+
+
+class TagFactory(DjangoModelFactory):
+
+    class Meta:
+        model = "recipe.Tag"
+
+    name = factory.Faker('sentence', nb_words=3)
+    user = factory.SubFactory(UserFactory)
