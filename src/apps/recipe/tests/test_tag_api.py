720eae53b51953a0f59db079c4e28ca33eeb22b1
TATO <tatososelia93@gmail.com>
2024-01-24 19:15:15 +0400

    feat: add tests for Tag API

---
+++ src/apps/recipe/tests/test_tag_api.py
@@ -0,0 +1,63 @@
+"""
+Tests for the Tag API.
+"""
+from django.test import TestCase
+from django.urls import reverse
+
+from apps.user.factories import UserFactory
+from apps.recipe.factories import TagFactory
+from apps.recipe.serializers import TagSerializer
+from apps.recipe.models import Tag
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+TAG_URL = reverse('recipe:tag-list')
+
+
+class PublicTagApiTests(TestCase):
+    """Test unauthenticated api requests."""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        """Test that login is required for retrieving."""
+        res = self.client.get(TAG_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateTagApiTest(TestCase):
+    """Test the authenticated api requests."""
+
+    def setUp(self):
+        self.user = UserFactory.create()
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_tags(self):
+        """Test retrieving a list of tags."""
+        TagFactory.create_batch(3, user=self.user)
+
+        res = self.client.get(TAG_URL)
+
+        tags = Tag.objects.all().order_by('-id')
+        serializer = TagSerializer(tags, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_tags_limited_to_user(self):
+        """Test retrieving tags for authenticated user is limited to user."""
+        other_user = UserFactory.create()
+        tag = TagFactory.create_batch(1, user=self.user)[0]
+        TagFactory.create_batch(3, user=other_user)
+
+        res = self.client.get(TAG_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res), 1)
+        self.assertEqual(res.data[0]['name'], tag.name)
+        self.assertEqual(res.data[0]['id'], tag.id)
+        self.assertEqual(res.data[0]['user'], tag.user)
