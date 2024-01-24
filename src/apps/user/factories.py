3e281a17352f17bf5ed6eb1bb221a4251c716e0a
TATO <tatososelia93@gmail.com>
2024-01-24 18:42:57 +0400

    chore: Update code in UserFactory to use string reference for model

--- src/apps/user/factories.py
+++ src/apps/user/factories.py
@@ -1,11 +1,10 @@
 import factory
 from factory.django import DjangoModelFactory
-from django.contrib.auth import get_user_model
 
 
 class UserFactory(DjangoModelFactory):
     class Meta:
-        model = get_user_model()
+        model = 'user.User'
 
     email = factory.Faker("email")
     password = factory.Faker("password")
