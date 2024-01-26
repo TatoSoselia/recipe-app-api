2feaacf4a2519984460e553c0d51239c497d914d
TATO <tatososelia93@gmail.com>
2024-01-26 17:53:20 +0400

    feat: add ingredient to admin site

--- src/apps/recipe/admin.py
+++ src/apps/recipe/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from apps.recipe.models import Recipe, Tag
+from apps.recipe.models import Recipe, Tag, Ingredient
 
 admin.site.register(Recipe)
 admin.site.register(Tag)
+admin.site.register(Ingredient)
