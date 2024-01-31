4c958d9caf82bd2724074c4ecf5c4a5fd2bcf0c1
TATO <tatososelia93@gmail.com>
2024-01-31 14:15:16 +0400

    feat: add image field

--- src/apps/recipe/models.py
+++ src/apps/recipe/models.py
@@ -1,8 +1,19 @@
 """
 Recipe models.
 """
+import uuid
+import os
+
 from django.db import models
 from config import settings
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image."""
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+
+    return os.path.join('uploads', 'recipe', filename)
 
 
 class Recipe(models.Model):
@@ -17,6 +28,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
